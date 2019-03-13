60e46329b2ad0d457f2ab184c2787d05dbf03f86
Слава Иевлев <slavaievlev9@gmail.com>
2019-03-13 14:34:25 +0400

    Added EntityFramework

--- RepairWorkSoftwareModel/Customer.cs
+++ RepairWorkSoftwareModel/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,10 @@ namespace RepairWorkSoftwareModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string CustomerFIO { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual List<Order> Orders { get; set; }
     }
 }
