4dd4217b6f067bdf30f986a9aa2fa7d958c129af
Слава Иевлев <slavaievlev9@gmail.com>
2019-04-20 14:57:28 +0400

    Added create customer window

---
+++ RepairWorkSoftwareWeb/Controllers/CustomerRestController.cs
@@ -0,0 +1,41 @@
+﻿using RepairWorkSoftwareDAL.BindingModel;
+using RepairWorkSoftwareDAL.Interface;
+using RepairWorkSoftwareDAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RepairWorkSoftwareWeb.Controllers
+{
+    public class CustomerRestController : ApiController
+    {
+        private ICustomerService customerService;
+
+        public CustomerRestController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+        public HttpResponseMessage Post(CustomerViewModel model)
+        {
+            HttpResponseMessage response;
+
+            if (string.IsNullOrEmpty(model.CustomerFIO))
+            {
+                response = Request.CreateResponse<CustomerViewModel>(System.Net.HttpStatusCode.Forbidden, model);  // TODO
+                return response;
+            }
+
+            customerService.AddElement(new CustomerBindingModel
+            {
+                CustomerFIO = model.CustomerFIO
+            });
+
+            response = Request.CreateResponse<CustomerViewModel>(System.Net.HttpStatusCode.Created, model);
+            return response;
+        }
+    }
+}
--- RepairWorkSoftwareWeb/Controllers/OrderRestController.cs
+++ RepairWorkSoftwareWeb/Controllers/OrderRestController.cs
@@ -1,4 +1,5 @@
-﻿using RepairWorkSoftwareDAL.Interface;
+﻿using RepairWorkSoftwareDAL.BindingModel;
+using RepairWorkSoftwareDAL.Interface;
 using RepairWorkSoftwareDAL.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -21,9 +22,48 @@ namespace RepairWorkSoftwareWeb.Controllers
 
         public HttpResponseMessage Post(OrderViewModel model)
         {
-            
+            HttpResponseMessage response;
 
-            var response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Created, model);
+            if (string.IsNullOrEmpty(model.CustomerId))
+            {
+                response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Forbidden, model);  // TODO
+                return response;
+            }
+
+            if (string.IsNullOrEmpty(model.WorkId))
+            {
+                response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Forbidden, model);  // TODO
+                return response;
+            }
+
+            if (string.IsNullOrEmpty(model.Count))
+            {
+                response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Forbidden, model);  // TODO
+                return response;
+            }
+
+            if (string.IsNullOrEmpty(model.Sum))
+            {
+                response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Forbidden, model);  // TODO
+                return response;
+            }
+
+            try
+            {
+                orderService.CreateOrder(new OrderBindingModel
+                {
+                    CustomerId = Convert.ToInt32(model.CustomerId),
+                    WorkId = Convert.ToInt32(model.WorkId),
+                    Count = Convert.ToInt32(model.Count),
+                    Sum = Convert.ToInt32(model.Sum)
+                });
+            }
+            catch (Exception ex)
+            {
+                // TODO
+            }
+
+            response = Request.CreateResponse<OrderViewModel>(System.Net.HttpStatusCode.Created, model);
             return response;
         }
     }
